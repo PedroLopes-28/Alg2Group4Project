dfdadb27ff1dd572a2cd73b023b15fac0475d51c
Pedro <pl2828280@gmail.com>
2025-04-10 18:52:47 -0300

    feat: adiciona as variaveis de troca e comparação as funções de ordenação

--- benchmark.h
+++ benchmark.h
@@ -13,22 +13,22 @@ int* geraQuaseOrdenados(int tam, int porcentagem);
 int* geraOrdenados(int tam, int ordem);
 
 //usa o método selectionSort para ordenar o vetor
-void selectionSort(int *vet, int tam);
+void selectionSort(int *vet, int tam, int *comparacao, int *troca);
 
 //usa o método insertionSort para ordenar o vetor
-void insertionSort(int* vet, int tam);
+void insertionSort(int* vet, int tam, int *comparacao, int *troca);
 
 //usa o método mergeSort para ordenar o vetor
 void mergeSort(int *vet, int inicio, int fim);
-void merge(int *vet, int inicio, int meio, int fim);
+void merge(int *vet, int inicio, int meio, int fim, int *comparacao, int *troca);
 
 //usa o método quicksort para ordenar o vetor
 void quickSort(int *vet, int inicio, int fim);
-int particiona(int *vet, int inicio, int fim);
+int particiona(int *vet, int inicio, int fim,int *comparacao, int *troca);
 
 //usa o método de heapSort para ordenar o vetor
-void heapSort(heap *h);
-void heapify(heap *h, int atual);
+void heapSort(heap *h,int *comparacao, int *troca);
+void heapify(heap *h, int atual,int *comparacao, int *troca);
 
 #define BENCHMARK_H
 
