9faadf9d1285cb14cb51b2a1377b090f4d47b59a
Pedro Henrique de Souza Lopes <pl2828280@gmail.com>
2025-04-10 18:34:29 -0300

    Feat: inclui os arquivos iniciais do projeto

---
+++ benchmark.c
@@ -0,0 +1,33 @@
+
+#include "benchmark.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct heap;
+
+//gera um vetor de int com numeros aleatórios
+int* geraAleatorios(int tam, int semente);
+
+//gera um vetor de int com numeros quase ordenados
+int* geraQuaseOrdenados(int tam, int porcentagem);
+
+//gera um vetor com os dados ordenados
+int* geraOrdenados(int tam, int ordem);
+
+//usa o método selectionSort para ordenar o vetor
+void selectionSort(int *vet, int tam);
+
+//usa o método insertionSort para ordenar o vetor
+void insertionSort(int* vet, int tam);
+
+//usa o método mergeSort para ordenar o vetor
+void mergeSort(int *vet, int inicio, int fim);
+void merge(int *vet, int inicio, int meio, int fim);
+
+//usa o método quicksort para ordenar o vetor
+void quickSort(int *vet, int inicio, int fim);
+int particiona(int *vet, int inicio, int fim);
+
+//usa o método de heapSort para ordenar o vetor
+void heapSort(heap *h);
+void heapify(heap *h, int atual);
---
+++ benchmark.h
@@ -0,0 +1,35 @@
+
+#ifndef BENCHMARK_H
+
+typedef struct heap heap;
+
+//gera um vetor de int com numeros aleatórios
+int* geraAleatorios(int tam, int semente);
+
+//gera um vetor de int com numeros quase ordenados
+int* geraQuaseOrdenados(int tam, int porcentagem);
+
+//gera um vetor com os dados ordenados
+int* geraOrdenados(int tam, int ordem);
+
+//usa o método selectionSort para ordenar o vetor
+void selectionSort(int *vet, int tam);
+
+//usa o método insertionSort para ordenar o vetor
+void insertionSort(int* vet, int tam);
+
+//usa o método mergeSort para ordenar o vetor
+void mergeSort(int *vet, int inicio, int fim);
+void merge(int *vet, int inicio, int meio, int fim);
+
+//usa o método quicksort para ordenar o vetor
+void quickSort(int *vet, int inicio, int fim);
+int particiona(int *vet, int inicio, int fim);
+
+//usa o método de heapSort para ordenar o vetor
+void heapSort(heap *h);
+void heapify(heap *h, int atual);
+
+#define BENCHMARK_H
+
+#endif //BENCHMARK_H
